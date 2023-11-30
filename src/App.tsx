5d78b16943eea258ba24b35119e39612ce65ce96
이승학 <shl907@naver.com>
2023-11-30 20:45:29 +0900

    feat: 로그인 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./routes/Login";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import Home from "./routes/Home";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    width: 430px;
+    height: 800px;
+    background-color: beige;
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+`;
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/users/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: #16a114;
+  width: 100%;
+  height: 120px;
+`;
+
+export default function Header() {
+  return <Wrapper></Wrapper>;
+}
