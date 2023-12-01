f154ca6dc4e4c12f2391787f13c2921c9d8c7a23
Jiiiiiy <zhiying5@naver.com>
2023-12-01 21:33:12 +0900

    Revert "home"
    This reverts commit 810aa3f739f9756c8d3c6b9477b54b35df2fd41e.

--- public/module/addComma.tsx
+++
@@ -1,8 +0,0 @@
-const addCommaPrice = (price: number) => {
-   const addComma = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-   return (
-      addComma
-   )
-}
-
-export default addCommaPrice
