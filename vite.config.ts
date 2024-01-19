31532cc614acc66233b161e835f532a367989e15
dltmdgkr <shl907@naver.com>
2024-01-19 15:41:21 +0900

    fix: vercel 오류수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,17 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   // define: {
-//   //   global: {},
-//   // },
-
-//   // is not a constructor를 위해 추가한 코드
-//   build: {
-//     // commonjsOptions: { include: [] },
-//     sourcemap: true,
-//   },
-//   // optimizeDeps: {
-//   //   disabled: false,
-//   // },
-// });
-
-import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
-  optimizeDeps: {
-    exclude: ['js-big-decimal'],
+  plugins: [react()],
+  define: {
+    global: {},
   },
-  define: { _global: {} },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
+
   build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-      extensions: ['.js', '.cjs'],
-      strictRequires: true,
-      transformMixedEsModules: true,
-    },
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
   },
 });
